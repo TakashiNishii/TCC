8f154df123ba72f29140adf74541181efdc1e4a7
unknown <ygortn14@gmail.com>
2023-01-18 13:55:41 -0400

    :art: Sobre nós

--- antipatternsProject/src/components/header.tsx
+++ antipatternsProject/src/components/header.tsx
@@ -139,7 +139,7 @@ export default function Header() {
                         <a href="#/contato" className="text-base font-medium text-gray-500 hover:text-gray-900">
                             Dúvidas
                         </a>
-                        <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
+                        <a href="#/sobreNos" className="text-base font-medium text-gray-500 hover:text-gray-900">
                             Sobre nós
                         </a>
 
