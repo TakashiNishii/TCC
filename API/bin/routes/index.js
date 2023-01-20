4bac713784fb7811485232be92e573ef73490caa
unknown <ygortn14@gmail.com>
2023-01-20 15:19:48 -0400

    :wrench: Build API

---
+++ API/bin/routes/index.js
@@ -0,0 +1,10 @@
+const Semana = require('../../models/semana');
+const database = require('../../db');
+const express = require('express');
+
+const router = express.Router();
+router.get('/', async function (req, res, next) {
+    var resultados = await Semana.findAll();
+    res.status(200).send(resultados);
+});
+module.exports = router;
