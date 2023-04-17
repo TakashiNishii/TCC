fad3dde588c485086949b8ad4694c1a9194256bb
TakashiNishii <ygortn14@gmail.com>
2023-04-17 19:12:26 -0300

    :tada: Semana 6

--- antipatternsProject/src/services/api.ts
+++ antipatternsProject/src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-    // baseURL: "https://antipatternsapi.onrender.com",
-    baseURL: "http://192.168.0.181:3000",
+    baseURL: "https://antipatternsapi.onrender.com",
+    // baseURL: "http://192.168.0.181:3000",
 });
 
 export default api;
